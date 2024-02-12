e0fc584623c8bae9ef7148535df654249f0f2e4e
Malthusis <58402624+Malthusis@users.noreply.github.com>
2024-02-11 21:58:29 -0600

    First draft of soul orbs/SP

--- src/components/playerHPBar.vue
+++ src/components/playerHPBar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="info_hp_bar_outline">
         <div id="info_player_hp_bar_solid" class="hp_bar_background"></div>
+        <div class="soul_orb"></div>
+        <div class="soul_orb spent"></div>
         {{ displayDecimal(player.getHpCurr) + " / " + displayDecimal(player.getHpMax) }}
     </div>
 </template>
@@ -15,5 +17,20 @@ const player = usePlayer();
 <style>
     #info_player_hp_bar_solid {
         width: v-bind("player.playerHpRatio")
+    }
+    .soul_orb {
+        background-image: radial-gradient(aqua,rgb(0,153,255));
+        min-height:16px;
+        min-width:16px;
+        top:20px;
+        position:absolute;
+        z-index: 2;
+        border-radius: 10px;
+        border: solid rgb(0, 55, 92) 1px;
+        margin-right: 90%
+    }
+    .spent {
+        background-color: rgb(71,71,71) !important;
+        margin-right: 75% !important;
     }
 </style>
